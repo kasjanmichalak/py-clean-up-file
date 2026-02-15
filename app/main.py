99cc8498c7e11892205f5a2f26ef1f6203420365
Kasjan Michalak <kasjan.michalak@gmail.com>
2026-02-15 21:47:41 +0000

    2save

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
 import os
 
+from typing import Optional, Type
+
+from types import TracebackType
+
 
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> None:
+    def __enter__(self) -> "CleanUpFile":
         with open(self.filename, "w") as f:
-            f.write("Hello World")
+            f.write(self.filename)
+            return self
 
-    def __exit__(self) -> None:
-        os.remove(self.filename)
+    def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_val: Optional[BaseException],
+            exc_tb: Optional[TracebackType],
+    ) -> None:
+        try:
+            os.remove(self.filename)
+        except FileNotFoundError:
+            pass
