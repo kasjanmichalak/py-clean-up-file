3ba777130bab2656d1c8ee432c2e7f1ec3722b70
Kasjan Michalak <kasjan.michalak@gmail.com>
2026-02-15 20:52:09 +0000

    1save

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> None:
+        with open(self.filename, "w") as f:
+            f.write("Hello World")
+
+    def __exit__(self) -> None:
+        os.remove(self.filename)
